4b92a7336e89034f2b2f41dd8f7220e1a0e70dfc
Tim Redick <t.redick@irt.rwth-aachen.de>
2023-01-05 17:31:56 +0100

    Squash CI commits

--- src/PoseErrors.jl
+++ src/PoseErrors.jl
@@ -1,6 +1,10 @@
 module PoseErrors
 
-# Matching the points 
+# Matching the points in ADD-S & MDD-S
 using NearestNeighbors
+# For projection based methods
+using SciGL
+
+foo() = true
 
 end # module PoseErrors
---
+++ test/runtests.jl
@@ -0,0 +1,9 @@
+# @license BSD-3 https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2022, Institute of Automatic Control - RWTH Aachen University
+# All rights reserved. 
+
+using PoseErrors
+using Test
+
+# TODO
+@test PoseErrors.foo()
